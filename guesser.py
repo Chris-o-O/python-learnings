d113eed255ffe28d3b79d21c0b6f18a98623536c
Chris-o-O <30440558+Chris-o-O@users.noreply.github.com>
2024-06-05 21:43:20 -0400

    exercise done

--- guesser.py
+++ guesser.py
@@ -1,9 +1,10 @@
 import random
 
 try:
-    top_range = int(input("Type a number:"))
+    top_range = int(input("Type a number: "))
 except ValueError:
-     print("a number please")
+     print("type in a number next time :)")
+     quit()
 
 while True:
     if top_range <= 0:
@@ -14,4 +15,30 @@ while True:
 
 random_number = random.randint(1,top_range)
 
-print(random_number)
+# we keep track of all guesses made
+guesses = 0
+
+while True:
+    # guesses tracker
+    guesses += 1
+
+    # user makes a guess
+    user_guess = int(input(f"Guess the number - between 1 and {top_range} :  "))
+
+    # we check if guess is correct or not
+    if user_guess == random_number:
+        print("Got it")
+        break 
+
+    # we indicate if it's below or above
+    elif user_guess < random_number:
+        print("you were below")
+    else: 
+        print("you were above")
+
+    # we congratulate the user when they find it
+print("You guessed in",guesses,"guesses. Nice")
+
+
+
+    
